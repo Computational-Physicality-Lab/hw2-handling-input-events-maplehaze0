7f5247e57c80a8c6b2c45d7fef8da8975c391901
maplehaze0 <kevinlimh9000@gmail.com>
2023-04-05 13:49:42 +0800

    t

--- input.js
+++ input.js
@@ -152,12 +152,15 @@ function Drag(e, box) {
 
     if ('ontouchstart' in window) {
 
-      currentPosX = e.touches[0].pageX;
-      currentPosY = e.touches[0].pageY;
-      var deltaX = currentPosX - startPosX;
-      var deltaY = currentPosY - startPosY;
+      // currentPosX = e.touches[0].pageX;
+      // currentPosY = e.touches[0].pageY;
+      var deltaX = e.touches[0].pageX - currentPosX;
+      var deltaY = e.touches[0].pageY - currentPosY;
       box.style.top = (startPosY  + deltaY) + "px";//control Y
       box.style.left = (startPosX + deltaX) + "px";//control X
+      currentPosX = e.touches[0].pageX;
+      currentPosY = e.touches[0].pageY;
+      
       console.log(box.style.top, box.style.left);
       console.log(deltaX, deltaY);
     } else {
